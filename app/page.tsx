e3a88d5c53365fdd032fcd5b2dda1e48e5c240ff
Sphurti <sphurtirdixit09@gmail.com>
2026-07-02 18:34:15 +0530

    fixes p-2 for deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import { brillant } from '../app/fonts'
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { brillant } from "../app/fonts";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -11,13 +11,20 @@ export default function Home() {
       }}
     >
       <div className="p-8 rounded-xl">
-        <h1 className={`${brillant.className} text-5xl font-bold text-white flex justify-center`}>
-          Sphurti <span className='text-purple-600'>Dixit</span>
+        <h1
+          className={`${brillant.className} text-5xl font-bold text-white flex justify-center`}
+        >
+          Sphurti <span className="text-purple-600">Dixit</span>
         </h1>
-        <p className="mt-4 text-xl flex justify-center">I craft beautiful, functional web experiences.</p>
-        <div className='flex col-1 sm:col-2 justify-center py-5 gap-6'>
+        <p className="mt-4 text-xl flex justify-center">
+          I craft beautiful, functional web experiences.
+        </p>
+        <div className="flex col-1 sm:col-2 justify-center py-5 gap-6">
           <div className="bg-gradient-to-r from-purple-500 to-pink-500 p-[2px] rounded-full hover:shadow-[0_0_20px_#a855f7] transition duration-300 w-fit">
-            <a href="/cvs/Sphurti_CV.pdf" download="Sphurti_Dixit_Resume.pdf">
+            <a
+              href="/cvs/Sphurti_Dixit_CV.pdf"
+              download="Sphurti_Dixit_Resume.pdf"
+            >
               <Button className="bg-black text-white font-semibold rounded-full px-6 py-2 hover:bg-black">
                 Download Resume
               </Button>
@@ -25,7 +32,9 @@ export default function Home() {
           </div>
           <Link href="/projects">
             <div className="bg-gradient-to-r from-purple-500 to-pink-500 p-[2px] rounded-full hover:shadow-[0_0_20px_#a855f7] transition duration-300 w-fit">
-              <Button className="bg-black text-white font-semibold rounded-full px-6 py-2 hover:bg-black">View Projects</Button>
+              <Button className="bg-black text-white font-semibold rounded-full px-6 py-2 hover:bg-black">
+                View Projects
+              </Button>
             </div>
           </Link>
         </div>
